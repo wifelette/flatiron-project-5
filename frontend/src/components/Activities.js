ef3e5a15a7b692bcad5809c55bdf0d75dc617ffb
Leah Silber <LeahSilber@gmail.com>
2020-06-11 13:18:24 -0700

    Stub first two components/pages for router setup

---
+++ frontend/src/components/Activities.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../App.css";
+
+export default function Activities() {
+  return (
+    <div>
+      <h1>Activities Page</h1>
+    </div>
+  );
+}
---
+++ frontend/src/components/Materials.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../App.css";
+
+export default function Materials() {
+  return (
+    <div>
+      <h1>Materials Page</h1>
+    </div>
+  );
+}
