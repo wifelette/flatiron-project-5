a439f81069362d55e7de6aa318f0ea00408f1e47
Leah Silber <LeahSilber@gmail.com>
2020-06-11 20:38:04 -0700

    Extract Nav logo to Logo Component

--- frontend/src/components/nav/Nav.js
+++ frontend/src/components/nav/Nav.js
@@ -1,8 +1,9 @@
 import React from "react";
-import "../App.css";
+import "../../App.css";
 import { Link } from "react-router-dom";
 import Divider from "./Divider";
 import NavItem from "./NavItem";
+import Logo from "./Logo";
 
 export default function Nav() {
   return (
@@ -13,15 +14,7 @@ export default function Nav() {
         id="accordionSidebar"
       >
         {/* Sidebar - Brand  */}
-        <Link
-          to="/"
-          className="sidebar-brand d-flex align-items-center justify-content-center a"
-        >
-          <div className="sidebar-brand-icon rotate-n-15">
-            <i className="fas fa-laugh-wink"></i>
-          </div>
-          <div className="sidebar-brand-text mx-3">Schedule</div>
-        </Link>
+        <Logo />
 
         <Divider />
 
@@ -30,36 +23,6 @@ export default function Nav() {
         </NavItem>
 
         <Divider />
-
-        {/* Nav Item - Kiddos Collapse Menu  */}
-        {/* <li className="nav-item">
-          <a
-            className="nav-link collapsed"
-            href="#"
-            data-toggle="collapse"
-            data-target="#collapseKids"
-            aria-expanded="true"
-            aria-controls="collapseKids"
-          >
-            <i className="fas fa-fw fa-user"></i>
-            <span>Kiddos</span>
-          </a>
-          <div
-            id="collapseKids"
-            className="collapse"
-            aria-labelledby="headingKids"
-            data-parent="#accordionSidebar"
-          >
-            <div className="bg-white py-2 collapse-inner rounded">
-              <a className="collapse-item" href="Kiddo1.html">
-                Child 1
-              </a>
-              <a className="collapse-item" href="Kiddo2.html">
-                Child 2
-              </a>
-            </div>
-          </div>
-        </li> */}
 
         <NavItem route="/activities" icon="bicycle">
           Activities
