bb73e100c5f44d64893a0f2401c04f2814ae0d1a
Leah Silber <LeahSilber@gmail.com>
2020-06-11 20:14:41 -0700

    Extract Router to Component

--- frontend/src/components/Routes.js
+++ frontend/src/components/Routes.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Activities from "./Activities";
 import Materials from "./Materials";
+import Dashboard from "./Dashboard";
 
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/" exact component="App" />
+      <Route path="/" exact component={Dashboard} />
       <Route path="/materials" exact component={Materials} />
       <Route path="/activities" exact component={Activities} />
     </Switch>
