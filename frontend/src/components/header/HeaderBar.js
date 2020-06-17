27aac88d0801dc455cb49e1c3c63e03842fc4964
Leah Silber <LeahSilber@gmail.com>
2020-06-17 16:36:15 -0700

    Add click listener to HeaderBar

--- frontend/src/components/header/HeaderBar.js
+++ frontend/src/components/header/HeaderBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FilterButton from "./FilterButton";
+import AddMaterialsForm from "../materials/AddMaterialsForm";
 
 /**
  *
@@ -7,10 +8,20 @@ import FilterButton from "./FilterButton";
  *  @param {string} props.headline
  *  @param {string} props.mainButton
  *  @param {FilterDetails} [props.filter]
+ *  @param { () => void } [props.onButtonClick]
  * @returns {JSX.Element}
  */
 
-export default function HeaderBar({ headline, mainButton, filter }) {
+export default function HeaderBar({
+  headline,
+  mainButton,
+  filter,
+  onButtonClick = () => {},
+}) {
+  // const showForm = () => {
+  //   return <AddMaterialsForm />;
+  // };
+
   return (
     <div className="container-fluid whole-page" style={{ paddingRight: "0px" }}>
       <h1 className="h3 mb-4 text-gray-800 col-xl-10">
@@ -18,6 +29,7 @@ export default function HeaderBar({ headline, mainButton, filter }) {
         <button
           type="button"
           className="btn btn-outline-success float-right shadow"
+          onClick={onButtonClick}
         >
           {mainButton}
         </button>
