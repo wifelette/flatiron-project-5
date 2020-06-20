47c362951dfec960be055e422cb110ecbcb4adb5
Leah Silber <LeahSilber@gmail.com>
2020-06-19 17:12:49 -0700

    Materials Reducer, including Types

---
+++ frontend/src/reducers/materials.js
@@ -0,0 +1,53 @@
+/**
+ * @typedef {object} AddMaterialAction
+ * @property {"ADD_MATERIAL"} type
+ * @property {Material} item
+ *
+ * @typedef {object} RemoveMaterialAction
+ * @property {"REMOVE_MATERIAL"} type
+ * @property {string} id
+ *
+ * @typedef {object} InitializeMaterialsAction
+ * @property {"INITIALIZE_MATERIALS"} type
+ * @property {Material[]} items
+ *
+ * @typedef {AddMaterialAction | RemoveMaterialAction | InitializeMaterialsAction} MaterialAction
+ *
+ * @typedef {Material[]} MaterialState
+ */
+
+/**
+ * @param {Material[] | undefined} state
+ * @param {MaterialAction} action
+ */
+export default (state = [], action) => {
+  switch (action.type) {
+    case "INITIALIZE_MATERIALS": {
+      return action.items;
+    }
+
+    case "ADD_MATERIAL": {
+      let newList = [...state, action.item];
+
+      newList.sort((a, b) => {
+        if (a.name < b.name) {
+          return -1;
+        } else if (a.name > b.name) {
+          return 1;
+        } else {
+          return 0;
+        }
+      });
+
+      return newList;
+    }
+
+    case "REMOVE_MATERIAL": {
+      return state.filter((item) => item.id !== action.id);
+    }
+
+    default: {
+      return state;
+    }
+  }
+};
