ca127724a50f35805dedd2d54ee54d468e59859f
Leah Silber <LeahSilber@gmail.com>
2020-06-19 17:14:55 -0700

    Import useSelector, useDispatch, add Types,

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,40 @@
+/// <reference path="./types/app.d.ts" />
+
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createStore } from "redux";
+import { createStore, Store } from "redux";
 import { allReducers } from "./reducers"; // No need to explicitly name Index.js
-import { Provider } from "react-redux";
+import {
+  Provider,
+  useSelector as useReduxSelector,
+  useDispatch as useReduxDispatch,
+} from "react-redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 // composeWithDevTools is just a cleaner way of getting the Redux browser extension to work. You pass it in as a second parameter to your store.
+
+/**
+ * @typedef {import("./reducers").AppState} AppState
+ * @typedef {import("./reducers").AppAction} AppAction
+ */
+
+/**
+ * https://react-redux.js.org/using-react-redux/static-typing
+ * @template T
+ * @typedef {import("react-redux").TypedUseSelectorHook<T>} TypedUseSelectorHook
+ */
+
+/** @type {Store<AppState, AppAction>} */
 const store = createStore(allReducers, composeWithDevTools());
+
+/** @type {TypedUseSelectorHook<AppState>} */
+export const useSelector = useReduxSelector;
+
+/** @type {() => typeof store.dispatch} */
+export const useDispatch = useReduxDispatch;
 
 ReactDOM.render(
   <Provider store={store}>
